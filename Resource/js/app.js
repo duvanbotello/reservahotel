112160916ea6e16eb46036d205b819a143b87422
dbotello <dbotello@ab-sistemas.com>
2019-07-05 17:36:12 -0500

    Terminando de configurar estructura de proyecto

---
+++ Resource/js/app.js
@@ -0,0 +1,18 @@
+/**
+ * CODIGO DE CLIENTES
+ */
+var cliente = new Cliente();
+//creamos una funcion para optener los datos y enviarlos al servidor.
+var loginUser = () =>{
+    //optenemos los datos del formulario login.
+    //se traen los input del formulario con id "email" y "pasword"
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    //utilizamos el metodo loginuser de la clase usuario y le pasos el email y el password
+    cliente.loginUser(email,password);
+}
+
+//utilizando jquery
+$().ready(()=>{
+
+});
